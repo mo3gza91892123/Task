3cd5f82bee0fbb51acaa8f8c1c7858ea9946ca8f
mo3gza91892123 <145809199+mo3gza91892123@users.noreply.github.com>
2023-10-16 01:53:16 +0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -16,8 +16,9 @@ num_four.forEach(element =>{
     element.innerHTML = 70000;
 });
 num_faive[0].innerHTML = num_three[0].innerHTML - num_two[0].innerHTML //هامش الرابح
-num_faive[1].innerHTML = (num_three[1].innerHTML - num_two[1].innerHTML) + "%" //معدل هامش الربح
+num_faive[1].innerHTML = (num_two[1].innerHTML / num_three[1].innerHTML) * 100 + "%" //معدل هامش الربح
+let prsant = parseInt(num_faive[1].innerHTML.replace('%',''));
 num_faive[2].innerHTML = num_one[2].innerHTML / num_faive[0].innerHTML //حجم التعادل
-num_faive[3].innerHTML = num_one[3].innerHTML / num_faive[0].innerHTML * 100  // قيمة التعادل
+num_faive[3].innerHTML = num_one[3].innerHTML / prsant * 100  // قيمة التعادل
 num_faive[4].innerHTML = (parseInt(num_one[4].innerHTML) + parseInt(num_four[4].innerHTML)) / num_faive[0].innerHTML //حجم المبيعات
-num_faive[5].innerHTML = (parseInt(num_one[5].innerHTML) + parseInt(num_four[5].innerHTML)) / num_faive[0].innerHTML * 100  // قيمة المبيعات
+num_faive[5].innerHTML = (parseInt(num_one[5].innerHTML) + parseInt(num_four[5].innerHTML)) / prsant * 100  // قيمة المبيعات
